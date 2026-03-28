35a66b90fe13317a0cdc84fde60a0a928991bb39
jasonvirtual2-cell <jasonvirtual2@gmail.com>
2026-03-28 12:42:18 +0100

    Add main React component for video editor

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Video Editor</h1>
+      {/* Add your video editor components here */}
+    </div>
+  );
+};
+
+export default App;
