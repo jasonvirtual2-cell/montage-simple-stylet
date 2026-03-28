dc12ab16c7e376ecae561e8e9d0768778111d0c6
jasonvirtual2-cell <jasonvirtual2@gmail.com>
2026-03-28 12:41:57 +0100

    Add Timeline component for video timeline

---
+++ src/components/Timeline.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Timeline = () => {
+    return (
+        <div className="timeline">
+            <h2>Video Timeline</h2>
+            {/* Timeline Contents */}
+        </div>
+    );
+};
+
+export default Timeline;
