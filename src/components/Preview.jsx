011b74f723c98827d5959e7565e42c55b89550bb
jasonvirtual2-cell <jasonvirtual2@gmail.com>
2026-03-28 12:42:04 +0100

    Create Preview component

---
+++ src/components/Preview.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Preview = () => {
+  return (
+    <div>
+      <h1>Preview Component</h1>
+      <p>This is a preview component.</p>
+    </div>
+  );
+};
+
+export default Preview;
